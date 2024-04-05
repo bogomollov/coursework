6c646cbfc5615cfe9fe78ad92a18cc14a5d18a95
bogomollov <bogdan.bogomolov.2016@mail.ru>
2024-04-05 15:29:18 +0500

    base

--- web/index.php
+++ web/index.php
@@ -7,10 +7,31 @@
 	<meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="favicon.ico"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="style.css"/>
     <title>Главная</title>
 </head>
 <body>
+    <div class="body-wrapper">
+        <header>
+            <div class="header-menu">
+                <div class="logo">
+                    <img src="./images/icon.png" width="64" height="64">
+                </div>
+                <div class="vertical-line"></div>
+                <div class="virtual-servers">
+                    <a href="#">Виртуальные сервера</a>
+                    <img src="./images/arrow.svg" width="10" height="10">
+                </div>
+            </div>
+            <div class="header-account">
+
+            </div>
+        </header>
+    </div>
 </body>
 </html>
