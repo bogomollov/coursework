44f6a9436d060030a66e62c69a59a40a79057017
bogomollov <bogdan.bogomolov.2016@mail.ru>
2024-04-19 15:28:33 +0500

    edit admin panel

--- web/admin.php
+++ web/admin.php
@@ -1,3 +1,9 @@
+<?php
+    include('db.php');
+    $orders = $db->prepare("SELECT * FROM Orders");
+    $orders->execute();
+    $rows = $orders->fetchAll(PDO::FETCH_ASSOC);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,33 +12,22 @@
     <title>Заявки</title>
 </head>
 <body>
-<table>
-    <tr>
-        <td>ID заявки</td>
-        <td>Тариф</td>
-        <td>ID клиента</td>
-        <td>E-mail клиента</td>
-    </tr>
-    <tr>
-        <td>id1</td>
-        <td>id2</td>
-        <td>id3</td>
-    </tr>
-    <tr>
-        <td>tarif1</td>
-        <td>tarif2</td>
-        <td>tarif3</td>
-    </tr>
-    <tr>
-        <td>client1</td>
-        <td>client2</td>
-        <td>client3</td>
-    </tr>
-    <tr>
-        <td>email1</td>
-        <td>email2</td>
-        <td>email3</td>
-    </tr>
-</table>
+<div class="orders-window">
+    <div class="orders-content">
+        <div class="title">
+            <a>ID заявки</a>
+            <a>ID клиента</a>
+            <a>E-mail клиента</a>
+            <a>Тариф</a>
+        </div>
+        <div class="orders-info">
+            <?php
+            foreach ($rows as $key => $row) {
+                echo $row['id'] . $row['client'] . $row['email'] . $row['tarif'] . '<br>';
+            }
+            ?>
+        </div>
+    </div>
+</div>
 </body>
 </html>
